65645746ec73201167a4e466d708426d90ce91a9
Evgeniy Shapovalov <ev.shapovalov@vk.team>
2025-07-06 07:39:27 +0300

    class highlighting and code view for js

--- src/ui/views/js-view.ts
+++ src/ui/views/js-view.ts
@@ -3,6 +3,70 @@ import { renderErrorPre, renderValue } from "ui/render";
 import { DataviewRefreshableRenderer } from "ui/refreshable-view";
 import { DataviewApi } from "api/plugin-api";
 
+import { EditorState, Range } from "@codemirror/state";
+import { Decoration, DecorationSet, EditorView, ViewPlugin, ViewUpdate } from "@codemirror/view";
+import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
+import { javascript } from "@codemirror/lang-javascript"
+
+import { tags } from "@lezer/highlight"
+
+// Plugin to highlight all lines
+const highlightAllLinesPlugin = ViewPlugin.fromClass(class {
+    decorations: DecorationSet;
+
+    constructor(view: EditorView) {
+        this.decorations = this.buildDecorations(view);
+    }
+
+    update(update: ViewUpdate) {
+        if (update.docChanged || update.viewportChanged) {
+            this.decorations = this.buildDecorations(update.view);
+        }
+    }
+
+    buildDecorations(view: EditorView): DecorationSet {
+        const builder: Range<Decoration>[] = [];
+        const { doc } = view.state;
+        for (let i = 1; i <= doc.lines; i++) {
+            const line = doc.line(i);
+            builder.push(Decoration.line({ class: "cm-s-obsidian HyperMD-codeblock" }).range(line.from));
+        }
+        return Decoration.set(builder, true);
+    }
+}, {
+  decorations: v => v.decorations
+});
+
+// ues classHighlighter?
+// @ts-ignore
+const cmHighlightStyle = HighlightStyle.define([
+  { tag: tags.link, class: "cm-link" },
+  { tag: tags.heading, class: "cm-heading" },
+  { tag: tags.emphasis, class: "cm-emphasis" },
+  { tag: tags.strong, class: "cm-strong" },
+  { tag: tags.keyword, class: "cm-keyword" },
+  { tag: tags.atom, class: "cm-atom" },
+  { tag: tags.bool, class: "cm-bool" },
+  { tag: tags.url, class: "cm-url" },
+  { tag: tags.labelName, class: "cm-label" },
+  { tag: tags.inserted, class: "cm-inserted" },
+  { tag: tags.deleted, class: "cm-deleted" },
+  { tag: tags.literal, class: "cm-literal" },
+  { tag: tags.string, class: "cm-string" },
+  { tag: tags.number, class: "cm-number" },
+  { tag: tags.variableName, class: "cm-variable" },
+  { tag: tags.typeName, class: "cm-typeName" },
+  { tag: tags.namespace, class: "cm-namespace" },
+  { tag: tags.className, class: "cm-className" },
+  { tag: tags.macroName, class: "cm-macroName" },
+  { tag: tags.propertyName, class: "cm-property" },
+  { tag: tags.operator, class: "cm-operator" },
+  { tag: tags.comment, class: "cm-comment" },
+  { tag: tags.meta, class: "cm-meta" },
+//  { tag: tags.punctuation, class: "cm-punctuation" },
+  { tag: tags.invalid, class: "cm-invalid" }
+]);
+
 export class DataviewJSRenderer extends DataviewRefreshableRenderer {
     static PREAMBLE: string = "const dataview = this;const dv = this;";
 
@@ -23,9 +87,34 @@ export class DataviewJSRenderer extends DataviewRefreshableRenderer {
 
         // Assume that the code is javascript, and try to eval it.
         try {
+            let div = this.container.createEl("div");
+
+            let code = this.container.createEl("div");
+            //code.innerHTML = previous;
+            // @ts-ignore
+            let editor = new EditorView({
+                parent: code,
+                doc: this.script, // "I am focusable but not editable"
+                extensions: [
+//                  basicSetup,
+                  javascript({typescript: true}),
+                  syntaxHighlighting(cmHighlightStyle),     // defaultHighlightStyle classHighlighter
+                  highlightAllLinesPlugin,
+                  EditorState.readOnly.of(true),
+                  EditorView.editable.of(false),
+                  EditorView.contentAttributes.of({tabindex: "0"})
+                ]
+              });
+            //editor.dom.addClasses(["cm-s-obsidian", "HyperMD-codeblock"]);
+
+            div.appendChild(code);
+
+            let resultContainer = this.container.createEl("div");
+            div.appendChild(resultContainer);
+
             await asyncEvalInContext(
                 DataviewJSRenderer.PREAMBLE + this.script,
-                new DataviewInlineApi(this.api, this, this.container, this.origin)
+                new DataviewInlineApi(this.api, this, resultContainer, this.origin)
             );
         } catch (e) {
             this.containerEl.innerHTML = "";
@@ -33,6 +122,8 @@ export class DataviewJSRenderer extends DataviewRefreshableRenderer {
         }
     }
 }
+
+
 
 /** Inline JS renderer accessible using '=$' by default. */
 export class DataviewInlineJSRenderer extends DataviewRefreshableRenderer {
